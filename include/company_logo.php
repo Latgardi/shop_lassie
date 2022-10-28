10cf3e7f0cf12bef58d45ab5c66307febe40baed
Latgardi <saikinmirai@gmail.com>
2022-10-28 13:15:08 +0300

    initial commit

---
+++ include/company_logo.php
@@ -0,0 +1,3 @@
+<a href="/" class="header__logo logo">
+    <img src="/include/logo.png" class="logo__img" alt="">
+</a>
---
+++ include/company_phone_day.php
@@ -0,0 +1,5 @@
+<div class="header__col header__col_contacts">
+    <div class="contacts"><a href="tel:+74952150435" class="contacts__tel">8 495 215-04-35</a>
+        <div class="contacts__info">с 9:00 до 24:00 ежедневно</div>
+    </div>
+</div>
